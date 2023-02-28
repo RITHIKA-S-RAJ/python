591c89261123e1b162e0ddc21b71b08894f02923
RITHIKA-S-RAJ <126576111+RITHIKA-S-RAJ@users.noreply.github.com>
2023-02-28 18:57:22 +0530

    Add files via upload

---
+++ RITHI.py
@@ -0,0 +1,9 @@
+mark = int(input("Enter your mark: "))
+if mark > 24 :
+   print("you have passed th exam.")
+   if mark > 90 :
+     print("You have an A+")
+   else:
+        print("you don't have an A+")
+else:
+    print("FAILED")        
